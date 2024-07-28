f8b45f1a2892461ab06e5e4bf362ddd225a01421
SchuhBaum <SchuhBaum71@gmail.com>
2024-07-28 20:03:00 +0200

    v0.0.1

---
+++ ReadMe.md
@@ -0,0 +1,36 @@
+## HiddenHUD
+###### Version: 0.0.1
+This is a mod for Rogue Legacy 2.
+
+### Description
+Hides the player HUD and other elements like the minimap. Can be configured in the file `[...]/Rogue Legacy 2/BepInEx/config/SchuhBaum.HiddenHUD.cfg`.
+
+Here is a youtube video:  
+[https://www.youtube.com/watch?v=Edf0pixzmAo](https://www.youtube.com/watch?v=Edf0pixzmAo)
+
+### Installation
+0.1 Download the file `BepInEx_win_x64_5.4.23.2.zip` (or newer) from [here](https://github.com/BepInEx/BepInEx/releases).
+0.2 Extract its content in the folder `[Steam]/SteamApps/common/Rogue Legacy 2` (this is the folder that contains the file `Rogue Legacy 2.exe`)
+1. Download the file `HiddenHUD.zip` from [Releases](https://github.com/SchuhBaum/HiddenHUD/releases/tag/v0.0.1).
+2. Again, extract its content in the folder `[Steam]/SteamApps/common/Rogue Legacy 2`.
+3. Start the game as normal.
+
+You can check the logs for errors or if you have installed BepInEx and the mod correctly. There are two logs -- `[...]/Rogue Legacy 2/BepInEx/LogOutput.log` and `[User]/AppData/LocalLow/Cellar Door Games/Rogue Legacy 2/Player.log`.
+In the logs, you should see entries like `[Info:BepInEx] Loading [HiddenHUD 0.0.1]`.
+
+### Bug reports
+Please describe step-by-step how to reproduce the issue. You can post them here on GitHub under the tab `Issues`.
+
+### Source Code
+This mod uses the mod template from `wobat`:  
+[https://www.nexusmods.com/roguelegacy2/mods/24](https://www.nexusmods.com/roguelegacy2/mods/24)
+
+### Contact
+If you have feedback, you can message me on Discord `@schuhbaum` or write an email to SchuhBaum71@gmail.com.
+
+### License
+See the file LICENSE-MIT.
+
+### Changelog
+v0.0.1:
+- Initial release.
---
+++ SourceCode/MainMod.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using UnityEngine;
+using Wob_Common;
+
+namespace HiddenHUD;
+
+[BepInPlugin("SchuhBaum.HiddenHUD", "HiddenHUD", "0.0.1")]
+public class MainMod : BaseUnityPlugin {
+    // meta data
+    public static string mod_id = "HiddenHUD";
+    public static string author = "SchuhBaum";
+    public static string version = "v0.0.1";
+
+    // options
+    public static bool is_fairy_rule_visible         = true;
+    public static bool is_player_hud_visible         = false;
+    public static bool is_player_minimap_visible     = false;
+    public static bool is_relics_visible             = false;
+    public static bool is_enemy_hud_visible          = false;
+    public static bool is_now_entering_visible       = true;
+    public static bool is_objective_complete_visible = true;
+    public static bool is_global_timer_visible       = true;
+
+	// variables
+    public static bool is_initialized = false;
+
+    protected void Awake() {
+        if (is_initialized) return;
+        is_initialized = true;
+        WobPlugin.Initialise(this, this.Logger);
+
+        WobSettings.Add(new WobSettings.Boolean("Options", "FairyRule", "", is_fairy_rule_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "PlayerHUD", "", is_player_hud_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "PlayerMinimap", "", is_player_minimap_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "Relics", "", is_relics_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "EnemyHUD", "", is_enemy_hud_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "NowEntering", "", is_now_entering_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "ObjectiveComplete", "", is_objective_complete_visible));
+        WobSettings.Add(new WobSettings.Boolean("Options", "GlobalTimer", "", is_global_timer_visible));
+        WobPlugin.Patch();
+
+        is_fairy_rule_visible = WobSettings.Get("Options", "FairyRule", is_fairy_rule_visible);
+        is_player_hud_visible = WobSettings.Get("Options", "PlayerHUD", is_player_hud_visible);
+        is_player_minimap_visible = WobSettings.Get("Options", "PlayerMinimap", is_player_minimap_visible);
+        is_relics_visible = WobSettings.Get("Options", "Relics", is_relics_visible);
+        is_enemy_hud_visible = WobSettings.Get("Options", "EnemyHUD", is_enemy_hud_visible);
+        is_now_entering_visible = WobSettings.Get("Options", "NowEntering", is_now_entering_visible);
+        is_objective_complete_visible = WobSettings.Get("Options", "ObjectiveComplete", is_objective_complete_visible);
+        is_global_timer_visible = WobSettings.Get("Options", "GlobalTimer", is_global_timer_visible);
+
+        Debug.Log(mod_id + ": author "                        + author);
+        Debug.Log(mod_id + ": version "                       + version);
+        Debug.Log(mod_id + ": is_fairy_rule_visible "         + is_fairy_rule_visible);
+        Debug.Log(mod_id + ": is_player_hud_visible "         + is_player_hud_visible);
+        Debug.Log(mod_id + ": is_player_minimap_visible "     + is_player_minimap_visible);
+        Debug.Log(mod_id + ": is_relics_visible "             + is_relics_visible);
+        Debug.Log(mod_id + ": is_enemy_hud_visible "          + is_enemy_hud_visible);
+        Debug.Log(mod_id + ": is_now_entering_visible "       + is_now_entering_visible);
+        Debug.Log(mod_id + ": is_objective_complete_visible " + is_objective_complete_visible);
+        Debug.Log(mod_id + ": is_global_timer_visible "       + is_global_timer_visible);
+    }
+}
